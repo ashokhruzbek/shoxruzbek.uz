7ebca4828e9f11c06aa522db30b2b2aa6b374547
shokhruzbek <ashokhruzbek@gmail.com>
2025-06-26 13:16:52 +0500

    replace profile picture

--- app/hero-section/Hero.tsx
+++ app/hero-section/Hero.tsx
@@ -4,7 +4,7 @@ import { monaSans } from "../fonts/monaSans";
 import { motion } from "framer-motion";
 import { imageAnimation, bodyAnimation } from "../animations/animations";
 import AnimatedWords from "../animations/AnimatedWords";
-import profile from "../../public/profile.jpg";
+import profile from "../../public/new-logo.png";
 import {
   FaGithub,
   FaTelegramPlane,
