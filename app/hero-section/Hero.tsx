fc9107a4cab4c004ac28adb524108f965c11b383
shokhruzbek <ashokhruzbek@gmail.com>
2025-06-26 14:28:03 +0500

    some updating

--- app/hero-section/Hero.tsx
+++ app/hero-section/Hero.tsx
@@ -4,7 +4,7 @@ import { monaSans } from "../fonts/monaSans";
 import { motion } from "framer-motion";
 import { imageAnimation, bodyAnimation } from "../animations/animations";
 import AnimatedWords from "../animations/AnimatedWords";
-import profile from "../../public/new-logo.png";
+import profile from "../../public/main-logo.png";
 import {
   FaGithub,
   FaTelegramPlane,
